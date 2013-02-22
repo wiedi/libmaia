e03afed982cb85837d9a696753c980a9ab1db863
liuguangzhao <liuguangzhao@users.sf.net>
2013-02-22 17:46:08 +0000

    Simple Qt4 class emulator

--- maiaXmlRpcServerConnection.h
+++ maiaXmlRpcServerConnection.h
@@ -33,6 +33,39 @@
 #include <QtNetwork>
 #include "maiaFault.h"
 
+#if QT_VERSION >= 0x050000
+class QHttpRequestHeader
+{
+public:
+    explicit QHttpRequestHeader(QString headerString);
+    virtual ~QHttpRequestHeader() {}
+
+    bool isValid();
+    QString method();
+    uint contentLength() const;
+
+private:
+    QString mHeaderString;
+    QString mMethod;
+    QMap<QString, QString> mHeaders;
+};
+
+class QHttpResponseHeader
+{
+public:
+    explicit QHttpResponseHeader(int code, QString text);
+    virtual ~QHttpResponseHeader() {}
+    void setValue(const QString &key, const QString &value);
+    virtual QString toString() const;
+
+private:
+    int mCode;
+    QString mText;
+    QMap<QString, QString> mHeaders;
+};
+#endif
+
+
 class MaiaXmlRpcServerConnection : public QObject {
 	Q_OBJECT
 	
