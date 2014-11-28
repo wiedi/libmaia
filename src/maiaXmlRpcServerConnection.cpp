1e60b257e585bc6fe590604d5fb37362a24ddfb7
Anatoliy Golubev <darth.naihil@gmail.com>
2014-11-28 11:58:17 +0300

    fixes #6 (Error when calling void methods with qt5

--- src/maiaXmlRpcServerConnection.cpp
+++ src/maiaXmlRpcServerConnection.cpp
@@ -153,81 +153,103 @@ void MaiaXmlRpcServerConnection::parseCall(QString call) {
 }
 
 
-/*	taken from http://delta.affinix.com/2006/08/14/invokemethodwithvariants/
-	thanks to Justin Karneges once again :) */
-bool MaiaXmlRpcServerConnection::invokeMethodWithVariants(QObject *obj,
-			const QByteArray &method, const QVariantList &args,
-			QVariant *ret, Qt::ConnectionType type) {
+/*
+taken from http://delta.affinix.com/2006/08/14/invokemethodwithvariants/
+thanks to Justin Karneges once again :)
+*/
+bool MaiaXmlRpcServerConnection::invokeMethodWithVariants( QObject *obj,
+                                                           const QByteArray &method,
+                                                           const QVariantList &args,
+                                                           QVariant *ret,
+                                                           Qt::ConnectionType type )
+{
+    // QMetaObject::invokeMethod() has a 10 argument maximum
+    if( args.count() > 10 ) {
+        return false;
+    }
 
-	// QMetaObject::invokeMethod() has a 10 argument maximum
-	if(args.count() > 10)
-		return false;
+    QList<QByteArray> argTypes;
+    for( int n = 0; n < args.count(); ++n ) {
+        argTypes += args[n].typeName();
+    }
 
-	QList<QByteArray> argTypes;
-	for(int n = 0; n < args.count(); ++n)
-		argTypes += args[n].typeName();
+    // get return type
+    int metatype = 0;
+    QByteArray retTypeName = getReturnType(obj->metaObject(), method, argTypes);
+    if( !retTypeName.isEmpty() && retTypeName != "QVariant" ) {
+        metatype = QMetaType::type(retTypeName.data());
+        if( metatype == 0 ) {
+            // lookup failed
+            return false;
+        }
+    }
 
-	// get return type
-	int metatype = 0;
-	QByteArray retTypeName = getReturnType(obj->metaObject(), method, argTypes);
-	if(!retTypeName.isEmpty()  && retTypeName != "QVariant") {
-		metatype = QMetaType::type(retTypeName.data());
-		if(metatype == 0) // lookup failed
-			return false;
-	}
+    QGenericArgument arg[10];
+    for( int n = 0; n < args.count(); ++n ) {
+        arg[n] = QGenericArgument(args[n].typeName(), args[n].constData());
+    }
 
-	QGenericArgument arg[10];
-	for(int n = 0; n < args.count(); ++n)
-		arg[n] = QGenericArgument(args[n].typeName(), args[n].constData());
+    QGenericReturnArgument retarg;
+    QVariant retval;
+    if( metatype != 0 && retTypeName != "void" ) {
+        retval = QVariant(metatype, (const void *)0);
+        retarg = QGenericReturnArgument(retval.typeName(), retval.data());
+    }
+    else {
+        /* QVariant */
+        retarg = QGenericReturnArgument("QVariant", &retval);
+    }
 
-	QGenericReturnArgument retarg;
-	QVariant retval;
-	if(metatype != 0) {
-		retval = QVariant(metatype, (const void *)0);
-		retarg = QGenericReturnArgument(retval.typeName(), retval.data());
-	} else { /* QVariant */
-		retarg = QGenericReturnArgument("QVariant", &retval);
-	}
+    if( retTypeName.isEmpty() || retTypeName == "void" ) {
+        /* void */
+        if( !QMetaObject::invokeMethod(obj, method.data(), type,
+                                       arg[0], arg[1], arg[2], arg[3], arg[4],
+                                       arg[5], arg[6], arg[7], arg[8], arg[9]) ) {
+            return false;
+        }
+    }
+    else {
+        if( !QMetaObject::invokeMethod(obj, method.data(), type, retarg,
+                                       arg[0], arg[1], arg[2], arg[3], arg[4],
+                                       arg[5], arg[6], arg[7], arg[8], arg[9]) ) {
+            return false;
+        }
+    }
 
-	if(retTypeName.isEmpty()) { /* void */
-		if(!QMetaObject::invokeMethod(obj, method.data(), type,
-						arg[0], arg[1], arg[2], arg[3], arg[4],
-						arg[5], arg[6], arg[7], arg[8], arg[9]))
-			return false;
-	} else {
-		if(!QMetaObject::invokeMethod(obj, method.data(), type, retarg,
-						arg[0], arg[1], arg[2], arg[3], arg[4],
-						arg[5], arg[6], arg[7], arg[8], arg[9]))
-			return false;
-	}
+    if( retval.isValid() && ret ) {
+        *ret = retval;
+    }
+    return true;
 
-	if(retval.isValid() && ret)
-		*ret = retval;
-	return true;
-}
+} // bool invokeMethodWithVariants( QObject *obj, const QByteArray &method, const QVariantList &args, QVariant *ret, Qt::ConnectionType type )
 
-QByteArray MaiaXmlRpcServerConnection::getReturnType(const QMetaObject *obj,
-			const QByteArray &method, const QList<QByteArray> argTypes) {
-	for(int n = 0; n < obj->methodCount(); ++n) {
-		QMetaMethod m = obj->method(n);
+QByteArray MaiaXmlRpcServerConnection::getReturnType( const QMetaObject *obj,
+                                                      const QByteArray &method,
+                                                      const QList<QByteArray> argTypes )
+{
+    for( int n = 0; n < obj->methodCount(); ++n ) {
+        QMetaMethod m = obj->method(n);
 #if QT_VERSION >= 0x050000
-		QByteArray sig = m.methodSignature();
+        if( m.name() == method && m.parameterTypes() == argTypes ) {
+            return m.typeName();
+        }
 #else
-		QByteArray sig = m.signature();
-#endif
-		int offset = sig.indexOf('(');
-		if(offset == -1)
-			continue;
-		QByteArray name = sig.mid(0, offset);
-		if(name != method)
-			continue;
-		if(m.parameterTypes() != argTypes)
-			continue;
+        QByteArray sig = m.signature();
 
-		return m.typeName();
-	}
-	return QByteArray();
-}
+        int offset = sig.indexOf('(');
+        if( offset == -1 ) {
+            continue;
+        }
+
+        QByteArray name = sig.mid(0, offset);
+        if( name == method && m.parameterTypes() == argTypes ) {
+            return m.typeName();
+        }
+#endif
+    }
+    return QByteArray();
+
+} // QByteArray getReturnType( const QMetaObject *obj, const QByteArray &method, const QList<QByteArray> argTypes )
 
 /*
   simple Qt4 class emulater
